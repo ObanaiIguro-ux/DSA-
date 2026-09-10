270ddc63d3d3e075bbdd7ddaa5548f5ea0fb3efa
anshul upadhyay <anshulu87@gmail.com>
2026-09-10 22:08:24 +0530

    Time: 0 ms (100%), Space: 9.9 MB (76.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,26 +3,22 @@ public:
     bool isPalindrome(string s) {
         
         int left = 0;
-        int right = s.length() - 1;
+        int right = s.length()-1;
 
-        while (left < right) {
+        while(left<right) {
 
-            // Skip non-alphanumeric characters from left
-            while (left < right && !isalnum(s[left])) {
+            while(left<right && !isalnum(s[left])){
                 left++;
             }
 
-            // Skip non-alphanumeric characters from right
-            while (left < right && !isalnum(s[right])) {
+            while(left<right && !isalnum(s[right])){
                 right--;
             }
 
-            // Compare characters ignoring uppercase/lowercase
-            if (tolower(s[left]) != tolower(s[right])) {
+            if(tolower(s[left]) != tolower(s[right])){
                 return false;
             }
 
-            // Move both pointers inward
             left++;
             right--;
         }
